bc1ecf020edd98fc55122201f412c0bd561da06d
Linas M <linas@weblink.com.br>
2015-08-04 15:46:17 +0300

    aloha

--- includes/cookie.php
+++ includes/cookie.php
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"website.com","theme":"light-floating"};
+    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"<?php echo $footer_privacy_link; ?>","theme":"light-floating"};
 </script>
 
 <script type="text/javascript" src="//s3.amazonaws.com/cc.silktide.com/cookieconsent.latest.min.js"></script>
