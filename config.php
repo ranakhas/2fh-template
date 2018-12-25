0d14f02bafc967dbae1e44ef3ebaab3d4112f8fd
ranakhas <33785157+ranakhas@users.noreply.github.com>
2018-12-25 13:59:10 +0500

    Added Config File

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$mofhdw = array(
+     
+'title' => '2FH-Template', // Site title
+
+'domain' => strtolower(preg_replace('/^www\./' , '' , $_SERVER['HTTP_HOST'])), // Automatic Domain Recognition
+);
+?>
