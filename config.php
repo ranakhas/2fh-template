d1af4c43bee450b1838dceb0daafebdfb8d63c07
ranakhas <33785157+ranakhas@users.noreply.github.com>
2018-12-25 14:44:23 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+$mofhdw = array(
+'title' => '2FH-Template', // Site title
+
+'domain' => strtolower(preg_replace('/^www\./' , '' , $_SERVER['HTTP_HOST'])), // Automatic Domain Recognition
+
+'description' => 'Get Free Professional Web Hosting for your Website', // Site Description
+
+'logo' => 'https://domain.tld/img/logo.png', // Logo URL
+     
+'protocol' => 'https://', // it is https or http
+   
+'favicon' => '/img/favicon.ico', // your website favicon url
+     
+'affid' => '25705', // your ifastnet affiliate id
+
+'mail' => 'some@mofh.ooo', // email here
+     
+'facebook' => 'https://www.facebook.com/#', // your facebook link
+     
+'twitter' => 'https://twitter.com/#', // your twitter link
+     
+'google-plus' => 'https://plus.google.com/#', // your google plus link
+     
+'linkedin' => 'https://www.linkedin.com/in/#', // your linkedin lin
+
+'language' => 'en', // your language supported en
+
+);
+?>
